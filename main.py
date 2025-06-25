c89ffed4f1c1a94a7840f6c4fe99bab0534a2184
woflmao <jfkathler@gmail.com>
2025-06-25 10:18:56 -0700

    auth

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ PLAYLISTID = os.getenv('SPOTIFY_PLAYLIST_ID')
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
-sp = ''
 
 async def authenticate_user():
   auth = SpotifyOAuth(
@@ -34,7 +33,7 @@ async def authenticate_user():
     scope=['playlist-modify-public'],
     open_browser=False
   )
-  sp = spotipy.Spotify(oauth_manager=auth)
+  return spotipy.Spotify(oauth_manager=auth)
 
 async def add_song_to_playlist(song_url):
     # auth = SpotifyOAuth(
@@ -45,7 +44,7 @@ async def add_song_to_playlist(song_url):
     #   open_browser=False
     # )
     # sp = spotipy.Spotify(oauth_manager=auth)
-
+    sp = await authenticate_user()
     # Extract the song id from the song url
     song_id = song_url.split('track/')[1]
     song_id = song_id.split('?')[0]
